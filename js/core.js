93d2540477e1c75259730b3c83dd607cd0c5f171
Alexey Karpov <karpovad@yandex.ru>
2014-05-09 03:43:05 +0400

    Добавлены utils

--- js/core.js
+++ js/core.js
@@ -18,3 +18,61 @@ var core = (function () {
         init: init
     };
 })();
+
+var utils = (function () {
+
+    var extendDeep = function (parent, child) {
+        var i,
+            toStr = Object.prototype.toString,
+            astr = "[object Array]";
+
+        child = child || ((toStr.call(parent) === astr) ? [] : {});
+
+        for (i in parent) {
+            if (parent.hasOwnProperty(i)) {
+                if (typeof parent[i] === "object") {
+                    child[i] = (toStr.call(parent[i]) === astr) ? [] : {};
+                    extendDeep(parent[i], child[i]);
+                } else {
+                    child[i] = parent[i];
+                }
+            }
+        }
+        return child;
+    };
+
+    var isEqual = function (one, two) {
+        var i;
+
+        for(i in one) {
+            if(one.hasOwnProperty(i)) {
+                if(one[i] != two[i]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    };
+
+    var isAPartOf = function (array, object) {
+        var i,
+            arrayLength = array.length;
+
+        for (i = 0; i < arrayLength; i += 1) {
+            if(isEqual(array[i], object)) {
+                return true;
+            }
+        }
+
+        return false;
+    };
+
+    return {
+        extendDeep: extendDeep,
+
+        isEqual: isEqual,
+
+        isAPartOf: isAPartOf
+    }
+})();
